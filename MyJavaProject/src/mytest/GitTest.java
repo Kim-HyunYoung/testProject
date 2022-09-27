3334944d14103bc1c307f9b84c83c431488e89c9
제미소/김현영 <77140973+Kim-HyunYoung@users.noreply.github.com>
2022-09-27 16:08:04 +0900

    Revert "test2"

--- MyJavaProject/src/mytest/GitTest.java
+++ MyJavaProject/src/mytest/GitTest.java
@@ -4,7 +4,6 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("hi");
 
-		System.out.println("test2222");
 		System.out.println("test111111");
 
 		System.out.println("test4444");
