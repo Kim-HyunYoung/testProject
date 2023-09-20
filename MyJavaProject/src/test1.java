f15c9ee033b602518fb6a913cfe6d0e2ef980edd
Kim-HyunYoung <ekek0725@naver.com>
2023-09-20 15:24:11 +0900

    test1

--- MyJavaProject/src/test1.java
+++ MyJavaProject/src/test1.java
@@ -2,5 +2,7 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("테스트");
         System.out.println("테스트2333");
+
+        System.out.println("test1");
     }
 }
